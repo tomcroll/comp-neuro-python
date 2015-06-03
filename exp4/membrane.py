f7adf6613479251819a518ab5c34ac229e770608
tomcroll <tomcroll@gmail.com>
2015-06-04 00:13:42 +0100

    Incresed tstop (current time) and printed steady state V

--- exp4/membrane.py
+++ exp4/membrane.py
@@ -12,13 +12,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # input current
-I = 10 # nA
+I = 10  # nA
 
 # capacitance and leak resistance
 
-C = 0.1 # nF
-R = 100 # M ohms
-tau = R*C # = 0.1*100 nF-Mohms = 100*100 pF Mohms = 10 ms
+C = 0.1  # nF
+R = 100  # M ohms
+tau = R * C  # = 0.1*100 nF-Mohms = 100*100 pF Mohms = 10 ms
 print('C = %.3f nF' % C)
 print('R = %.3f M ohms' % R)
 print('tau = %.3f ms' % tau)
@@ -26,37 +26,42 @@ print('(Theoretical)')
 
 # membrane potential equation dV/dt = - V/RC + I/C
 
-tstop = 150 # ms
+tstop = 1500  # ms
 
-V_inf = I*R # peak V (in mV)
-tau = 0 # experimental (ms)
+V_inf = I * R  # peak V (in mV)
+tau = 0  # experimental (ms)
 
-h = 0.2 # ms (step size)
+h = 0.2  # ms (step size)
 
-V = 0 # mV
-V_trace = [V] # mV
+V = 0  # mV
+V_trace = [V]  # mV
+I_off = 0
+steady_state_V = 0
 
 for t in np.arange(h, tstop, h):
 
-   # Euler method: V(t+h) = V(t) + h*dV/dt
-   V = V +h*(- (V/(R*C)) + (I/C))
+    # Euler method: V(t+h) = V(t) + h*dV/dt
+    V = V + h * (- (V / (R * C)) + (I / C))
 
-   # Verify membrane time constant
-   if (not tau and (V > 0.6321*V_inf)):
-     tau = t
-     print('tau = %.3f ms' % tau)
-     print('(Experimental)')
+    # Verify membrane time constant
+    if (not tau and (V > 0.6321 * V_inf)):
+        tau = t
+        print('tau = %.3f ms' % tau)
+        print('(Experimental)')
 
-   
-   # Stop current injection 
-   if t >= 0.6*tstop:
-     I = 0
+    # Stop current injection
+    if t >= 0.6 * tstop:
+        I_off = 1
+        I = 0
 
-   V_trace += [V]
-   if t % 10 == 0:
-       plt.plot(np.arange(0,t+h, h), V_trace, color='r')
-       plt.xlim(0, tstop)
-       plt.ylim(0, V_inf)
-       plt.draw()
-       
+    if I_off == 0:
+        steady_state_V = V
+
+    V_trace += [V]
+    if t % 10 == 0:
+        plt.plot(np.arange(0, t + h, h), V_trace, color='r')
+        plt.xlim(0, tstop)
+        plt.ylim(0, V_inf)
+        plt.draw()
+print('V = %s mV' % steady_state_V)
 plt.show()
